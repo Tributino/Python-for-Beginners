609c4c1a2c76078cb634e076cb87bee6e2b2b17c
Ivaldo Tributino <38995665+Tributino@users.noreply.github.com>
2025-09-26 10:25:14 -0700

    Add files via upload

---
+++ Week_5/midTerm.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+movies = ['The Pianist', 'Meet John Doe', 'Mr. Smith Goes to Washington', 'It\'s a Wonderful Life', 
+          'Gone with the Wind', 'The Shawshank Redemption', 'The lord of the rings', 'The Hobbit',
+          'Sound of freedom', 'Schindler\'s List', 'Seabiscuit', 'Nefarious', 'The Magnificent Seven']
+
+students = '''Curi Pachas, Sofia Belinda (2342915); Leon Macias, Ava Sofia (2342514); Mora Silva, Valeria (2322596); Nguyen, Quoc Anh (2342768); Serrano Fragoso, Anais (2343003)'''
+
+
+
+def student_full_name(id):
+    st_name = None
+    # write your code here
+
+    #
+    return st_name
+
+    
